fa4cfeb0cdae5c7f243cd6ec4afe67ec211f5677
tkanade <tarun.kanade@capgemini.com>
2022-07-18 22:19:26 +0530

    Added setter injection and using properties file

--- spring-demo-one/src/com/spring/springDemo/CricketCoach.java
+++ spring-demo-one/src/com/spring/springDemo/CricketCoach.java
@@ -4,10 +4,40 @@ public class CricketCoach implements Coach {
 	
 	// Define a private field for the dependency
 	private FortuneService fortuneService;
+	
+	private String emailAddress;
+	private String team;
 		
 	// Define a constructor to inject dependency
-	public CricketCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
+//	public CricketCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	public CricketCoach() {
+		System.out.println("CricketCoach: iside no-arg constructor");
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: setter injection");
+		this.fortuneService = fortuneService;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter setTeam");
+		this.team = team;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 	@Override
