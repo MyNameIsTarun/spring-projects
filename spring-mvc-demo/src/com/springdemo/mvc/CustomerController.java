13d0b1a15d8c0cf7e507bd9d22fd27419388ef25
Tarun Kanade <tarunkanademhs@gmail.com>
2022-08-03 21:57:59 +0530

    Using @InitBinder to preprocess requests before reaching controller

--- spring-mvc-demo/src/com/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/springdemo/mvc/CustomerController.java
@@ -2,15 +2,27 @@ package com.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// remove whitespaces from start and end of string
+	// It's a preprocessor which processes web requests before reaching to controller
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		System.out.println("is working");
+		StringTrimmerEditor trimmerBinder = new StringTrimmerEditor(true);
+		binder.registerCustomEditor(String.class, trimmerBinder);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
