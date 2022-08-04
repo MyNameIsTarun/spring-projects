d0a1a226ea6d0f9957c0a4d83625163e924bd75b
Tarun Kanade <tarunkanademhs@gmail.com>
2022-08-04 21:57:11 +0530

    Making input field required

--- spring-mvc-demo/src/com/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/springdemo/mvc/Customer.java
@@ -10,13 +10,14 @@ public class Customer {
 
 	private String firstName;
 	
-	@NotNull()
+	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@NotNull(message="is required")
 	@Min(value=0, message="must be >= 0")
 	@Max(value=10, message="must be <= 10")
-	private String freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
@@ -37,11 +38,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public String getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(String freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
