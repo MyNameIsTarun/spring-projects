05efe132a689e449ae7af912345c5d1054b9d44e
Tarun Kanade <tarunkanademhs@gmail.com>
2022-09-13 21:25:25 +0530

    Created new project for Eager vs Lazy fetch type

---
+++ hb-eager-vs-lazy-demo/src/com/spring/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,43 @@
+package com.spring.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.spring.hibernate.demo.entity.Instructor;
+import com.spring.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			
+			InstructorDetail theInstructorDetail = session.get(InstructorDetail.class, 2);
+			
+			System.out.println(theInstructorDetail);
+			
+			System.out.println("\nAssociated Instructor: "+ theInstructorDetail.getInstructor());
+			
+			System.out.println("Done");
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
